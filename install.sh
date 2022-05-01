cb4a89c3cb52e8413ff43cf53bbffc104bbd4062
Hari Balamani <harib@thoughtworks.com>
2022-05-01 20:33:57 +1000

    fixing merge issues

--- install.sh
+++ install.sh
@@ -1,10 +1,6 @@
 echo 'You might need to change your default shell to zsh: `chsh -s /bin/zsh` (or `sudo vim /etc/passwd`)'
 
-<<<<<<< HEAD
-dir="$HOME/Development"
-=======
 dir="$HOME/Developer/delta"
->>>>>>> 46f12ab (changing the default path to delta)
 mkdir -p $dir
 cd $dir
 git clone --recursive git://github.com/hari-tw/dotfiles.git
